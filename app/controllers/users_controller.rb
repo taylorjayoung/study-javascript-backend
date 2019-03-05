2b65231be01113b17b3264e621a6d5a7e3e812fd
sinyoungnam <sinnam90@gmail.com>
2019-03-05 11:48:59 -0500

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,42 @@
 class UsersController < ApplicationController
 
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        # byebug
-        render(json: @user, status: :ok)
-      else
-        render json: {message: 'invalid'}
-      end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render(json: @user, status: :ok)
+    else
+      render json: {message: 'invalid'}
     end
+  end
 
-    def index
-      @users = User.all
-      render( {json: @users, include: [:questions, :collections], status: :ok} )
+  def index
+    @users = User.all
+    render( {json: @users, include: [:questions, :collections], status: :ok} )
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render( {json: @user, include: :questions,  status: :ok} )
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @user = req.params.body
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      render(json: @user, status: :ok)
+    else
+      render json: {message: 'invalid'}
     end
+  end
 
-    def show
-      @user = User.find(params[:id])
-      render( {json: @user, include: :questions,  status: :ok} )
-    end
+private
 
-    def edit
-      @user = User.find(params[:id])
-      @user = req.params.body
-    end
-
-
-
-    def update
-      @user = User.find(params[:id])
-      if @user.save
-  #Ruby Controller Validations , WE don't want free access to corrupt databse
-        render(json: @user, status: :ok)
-      else
-        render json: {message: 'invalid'}
-      end
-    end
-  private
-    def user_params
-       params.require(:user).permit(:username, :name, :password, :email)
-    end
+  def user_params
+     params.require(:user).permit(:username, :name, :password, :email)
+  end
 
 end
