f6a0349d9604562653f0ec49bd9d2246c4c8fde0
sinyoungnam <sinnam90@gmail.com>
2019-03-17 22:12:08 -0400

    comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   resources :collections
   resources :questions
   resources :sessions
+  resources :questions_collection
 
   get '/', to: 'application#standard'
   post'/signup', to: 'users#create'
 
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  # post '/category_question', to: 'questions#create'
+    # => question.create
+    # question_catoegry.create with the above id
 end
