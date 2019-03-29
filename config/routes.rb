617b34cac665d0bb87e112a66e11f0e4f3a149ab
sinyoungnam <sinnam90@gmail.com>
2019-03-29 13:12:28 -0400

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
   resources :collections
   resources :questions
   resources :sessions
-<<<<<<< HEAD
-  resources :questions_collection
-=======
   resources :questions_collections
->>>>>>> b9a84ee6964bce66504d27b3eb7247774de076ea
 
   get '/', to: 'application#standard'
   post'/signup', to: 'users#create'
@@ -16,10 +12,4 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-<<<<<<< HEAD
-  # post '/category_question', to: 'questions#create'
-    # => question.create
-    # question_catoegry.create with the above id
-=======
->>>>>>> b9a84ee6964bce66504d27b3eb7247774de076ea
 end
